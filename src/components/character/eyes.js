7cfd80940dcc5eec6ca60349c4c27658ecb794d4
Ben Redden <benjamminredden@gmail.com>
2020-08-29 13:47:57 -0400

    adding funky scrappy body svg for placeholder, moving eyes to separate component, minor css work

---
+++ src/components/character/eyes.js
@@ -0,0 +1,123 @@
+// @flow
+import React from "react";
+import { TweenLite } from "gsap";
+
+type State = {};
+
+type Props = {};
+
+class Eyes extends React.Component<Props, State> {
+  svg: any;
+  leftEye: any;
+  rightEye: any;
+  requestId: any;
+  mouse: any;
+  onMouseMove: any;
+  onFrame: any;
+  createEye: any;
+
+  constructor(props: any) {
+    super(props);
+
+    this.svg = null;
+    this.leftEye = null;
+    this.rightEye = null;
+    this.requestId = null;
+    this.mouse = null;
+    this.onMouseMove = this.onMouseMove.bind(this);
+    this.onFrame = this.onFrame.bind(this);
+    this.createEye = this.createEye.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("mousemove", this.onMouseMove);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("mousemove", this.onMouseMove);
+  }
+
+  onMouseMove(event: SyntheticMouseEvent<T>) {
+    this.mouse = this.svg.createSVGPoint();
+    this.mouse.x = event.clientX;
+    this.mouse.y = event.clientY;
+
+    if (!this.requestId) {
+      this.requestId = requestAnimationFrame(this.onFrame);
+    }
+  }
+
+  onFrame() {
+    var point = this.mouse.matrixTransform(this.svg.getScreenCTM().inverse());
+
+    this.leftEye = this.createEye("#left-eye");
+    this.rightEye = this.createEye("#right-eye");
+    this.leftEye.rotateTo(point);
+    this.rightEye.rotateTo(point);
+
+    this.requestId = null;
+  }
+
+  createEye(selector: string) {
+    var element = document.querySelector(selector);
+
+    TweenLite.set(element, {
+      transformOrigin: "center",
+    });
+
+    var bbox = element.getBBox();
+    var centerX = bbox.x + bbox.width / 2;
+    var centerY = bbox.y + bbox.height / 2;
+
+    function rotateTo(point: any) {
+      var dx = point.x - centerX;
+      var dy = point.y - centerY;
+
+      var angle = Math.atan2(dy, dx);
+
+      TweenLite.to(element, 0.3, {
+        rotation: angle + "_rad_short",
+      });
+    }
+
+    return {
+      element: element,
+      rotateTo: rotateTo,
+    };
+  }
+
+  render() {
+    return (
+      <svg
+        className="Eyes"
+        id="svg"
+        viewBox="0 0 1000 1000"
+        ref={(element) => {
+          this.svg = element;
+        }}
+      >
+        <g
+          id="left-eye"
+          ref={(element) => {
+            this.leftEye = element;
+          }}
+        >
+          <circle className="eye-outer" cx="400" cy="500" r="100" />
+          <circle className="eye-inner" cx="480" cy="500" r="20" />
+        </g>
+
+        <g
+          id="right-eye"
+          ref={(element) => {
+            this.rightEye = element;
+          }}
+        >
+          <circle className="eye-outer" cx="600" cy="500" r="100" />
+          <circle className="eye-inner" cx="680" cy="500" r="20" />
+        </g>
+      </svg>
+    );
+  }
+}
+
+export default Eyes;
